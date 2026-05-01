9262602dbb3d83726428300ca0c6b0c3421726f8
vyshu2714-commits <vyshu2714@gmail.com>
2026-05-01 11:31:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+import Toggle from "./components/Toggle";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Toggle />
+    </div>
+  );
+}
